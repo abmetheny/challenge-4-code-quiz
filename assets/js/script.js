2dddf168438b9bc6383719904ee59007f274a74d
abmetheny <abmetheny@gmail.com>
2023-01-21 17:44:03 -0500

    Set up timer variables and initial functionality, and removed unnecessary commented out code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,9 @@ var initialsText = document.getElementById("initials");
 var initialsList = document.getElementById("initialsList");
 var scoreText = document.getElementById("score");
 var scoreList = document.getElementById("scoreList");
+var timerContainer = document.getElementById("timerWrapper");
+var timerText = document.getElementById("timer");
+
 
 // Variables for question and answer rendering
 var allQuestions = [{
@@ -59,9 +62,45 @@ var scoreCount = 0;
 var initials = "";
 var highScore = "";
 
+// Variables for timer
+var timer;
+var timerCount;
+var isWin = false;
+
+// Functions for running the timer
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerText.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        timeUp();
+      }
+    }, 1000);
+}
+
+function timeUp() {
+    quizContainer.style.display = "none";
+    timerContainer.style.display = "none";
+    alert("Time's up!  Please try again.");
+    startButton.style.display = "block";
+}
+
 // Functions for question and answer rendering
 function startQuiz() {
     startButton.style.display = "none";
+    timerCount = 10;
+    startTimer();
     loadQuestion();
     quizContainer.style.display = "block";
 }
@@ -80,7 +119,6 @@ function checkAnswer(answer) {
         console.log("Correct!");
         scoreCount++;
         console.log(scoreCount);
-        // setScore();
         checkLength();
     }
     else {
@@ -96,6 +134,7 @@ function checkLength() {
     }
     else {
         console.log("There are no more questions.");
+        clearInterval(timer);
         quizContainer.style.display = "none";
         enterInitials();
     }
@@ -114,8 +153,6 @@ function displayInitials() {
     var highScoreKeys = Object.keys(storedInitials);
     var highScoreValues = Object.values(storedInitials);
 
-    // console.log(Object.keys(storedInitials));
-    // console.log(Object.values(storedInitials));
     scoreContainer.style.display = "block";
 
     for (let i = 0; i < highScoreKeys.length; i++) {
@@ -135,118 +172,10 @@ function displayInitials() {
         var liScore = document.createElement('li');
         liScore.appendChild(document.createTextNode(`${highScoreValues[i]}`));
         document.getElementById('score').appendChild(liScore);
-        // initialsList.append(`${highScoreKeys[i]}\r\n`);
-        // scoreList.append(`${highScoreValues[i]}\r\n`);
-
         
     }
 
 }
 
-
-
 // Event listeners
 startButton.addEventListener("click", startQuiz);
-
-
-
-// answer1.addEventListener("click", checkAnswer);
-// answer2.addEventListener("click", checkAnswer);
-// answer3.addEventListener("click", checkAnswer);
-// answer4.addEventListener("click", checkAnswer);
-
-
-// function populateQuiz(event) {
-//     var element = event.target.textContent;
-//     if (element) {
-//         loadNextQuestion();
-//     }
-// }
-
-// function loadNextQuestion() {
-    // for (var i = 0; i < allQuestions.length; i++) {
-    //     console.log(i, allQuestions[i]);
-    //     questionText = question.textContent = allQuestions[i].question;
-    //     answer1Text = answer1.textContent = allQuestions[i].allAnswers[0];
-    //     answer2Text = answer2.textContent = allQuestions[i].allAnswers[1];
-    //     answer3Text = answer3.textContent = allQuestions[i].allAnswers[2];
-    //     answer4Text = answer4.textContent = allQuestions[i].allAnswers[3];
-    // }
-    // return;
-// }
-
-// function checkAnswer(event) {
-//     var element = event.target.textContent;
-//     if (element) {
-//         if (element === allQuestions[i].correctAnswer) {
-//             console.log("correct!");
-//         }
-//         else {
-//             console.log("incorrect");
-//         } 
-//     }
-//     loadNextQuestion();
-// }
-
-
-// function setScore() {
-//     localStorage.setItem("score", scoreCount);
-// }
-
-// function displayScore() {
-//     var storedScore = localStorage.getItem("score");
-
-//     if (storedScore === null) {
-//         scoreCount = "";
-//     }
-//     else {
-//         scoreCount = storedScore;
-//     }
-
-//     scoreText.textContent = scoreCount;
-// }
-
-
-
-// Function to run the quiz
-
-    // Listen for click on start button
-
-        // Timer countdown begins
-
-        // Populate HTML fields with first set of "questions" attributes
-        
-        // Listen for click on one of the answers
-
-            // If they click on the correct answer, add 1 to total correct value
-
-            //If they click on the wrong answer, subtract time from the timer
-        
-        // HTML fields cleared and populated with the next question
-
-        // If final question is answered OR if timer = 0, end quiz
-
-        // Prompt user to input initials and store
-
-        // Display score and high scores (indicate which one is the one just finished by highlight?)
-
-
-
-
-// Question and answer object arrays
-
-// var questions = ["Commonly used data types DO NOT include:", "The condition in an if/else statement is enclosed in _________.", 
-// "Arrays in JavaScript can be used to store ________.", "String values must be enclosed in ________ when being assigned to variables.",
-// "A very useful tool used during development and debugging for printing content to the debugger is:",];
-// var answerAs = ["strings", "quotes", "numbers and strings", "commas", "JavaScript"];
-// var answerBs = ["booleans", "curly brackets", "other variables", "curly brackets", "terminal/bash"];
-// var answerCs = ["alerts", "parentheses", "booleans", "quotes", "for loops", ];
-// var answerDs = ["numbers", "square brackets", "all of the above", "parentheses", "console.log"];
-// var correcAnswers = ["alerts","parentheses", "all of the above", "quotes", "console.log"]
-
-
-// function whatever(tacocat){
-//     let isPalindrome = tacocat.split("").reverse().join("")
-
-// return (isPalindrome == tacocat) ? "true" : "false"
-// }
